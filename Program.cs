f5a4a377983a34e75ff9b8d9a17d21b2994c3302
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-04 13:31:27 -0700

    Checkpoint 2: Load from file

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@
 Console.Clear();
 Console.WriteLine("Welcome to a Maze Game!");
 Console.WriteLine("Navigate a maze with your arrow keys and try to reach the \"*\"!");
-Console.WriteLine("Avoid the bad guys '%' and collect the coins '^' for bonus points!");
+Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) for bonus points!");
 Console.Write("Press enter to start playing!  ");
 Console.ReadKey();
+
+// Clean up console for printing the map
+Console.Clear();
+
+// Get the map from the file and print it
+string[] mapRows = File.ReadAllLines("map.txt");
+foreach (string row in mapRows)
+{
+    Console.WriteLine(row);
+}
+Console.WriteLine("Navigate with your arrow keys to reach the \"*\"!");
+Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) for bonus points!");
--- obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c47849ea3887a9aa507931dde3c37d9bf83260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ff21e42b413bb2e205df74f352e90b99a3fd94")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
