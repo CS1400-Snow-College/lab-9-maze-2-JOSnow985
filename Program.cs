e1ff21e42b413bb2e205df74f352e90b99a3fd94
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-04 13:14:07 -0700

    Checkpoint 1: Comment and Description

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Jaden Olvera, 11-4-25, Lab 9 - Maze 2
+
+Console.Clear();
+Console.WriteLine("Welcome to a Maze Game!");
+Console.WriteLine("Navigate a maze with your arrow keys and try to reach the \"*\"!");
+Console.WriteLine("Avoid the bad guys '%' and collect the coins '^' for bonus points!");
+Console.Write("Press enter to start playing!  ");
+Console.ReadKey();
