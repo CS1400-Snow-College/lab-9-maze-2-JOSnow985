57ecbb1a5dcf1e4294f200813a90ab987a99a533
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-04 17:34:34 -0700

    Reworked map array

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 
 Console.Clear();
 Console.WriteLine("Welcome to a Maze Game!");
-Console.WriteLine("Navigate a maze with your arrow keys and try to reach the \"*\"!");
-Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) for bonus points!");
+Console.WriteLine("Navigate a maze with your arrow keys and try to reach the \"#\"!");
+Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) to unlock the vault!");
 Console.Write("Press enter to start playing!  ");
 Console.ReadKey();
 
@@ -11,13 +11,15 @@ Console.ReadKey();
 Console.Clear();
 
 // Get the map from the file and print it
-string[] mapRows = File.ReadAllLines("map.txt");
-foreach (string row in mapRows)
+char[][] mapCharArray = File.ReadAllLines("map.txt").Select(fileStrings => fileStrings.ToCharArray()).ToArray();
+foreach (char[] line in mapCharArray)
 {
-    Console.WriteLine(row);
+    foreach (char character in line)
+        Console.Write(character);
+    Console.WriteLine();
 }
-Console.WriteLine("Navigate with your arrow keys to reach the \"*\"!");
-Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) for bonus points!");
+Console.WriteLine("Navigate with your arrow keys to reach the \"#\"!");
+Console.WriteLine("Avoid the bad guys (%) and collect the coins (^) to unlock the vault!");
 
 // Set up an int array to hold the initial x and y coordinates
 int[] currentCoordinates = [0, 0];
@@ -26,25 +28,28 @@ Console.SetCursorPosition(currentCoordinates[0],currentCoordinates[1]);
 // Get ready to keep track of what the last key pressed was
 ConsoleKey lastKey;
 
+// Create a bool to track win or loss, true for win false for loss
+bool gameWon = false;
+
 // Main loop, playing on the maze already printed
 do {
     lastKey = Console.ReadKey(true).Key;
     switch (lastKey)
     {
         case ConsoleKey.UpArrow:
-            if (TryMove(currentCoordinates[0], currentCoordinates[1] - 1, mapRows) == true)
+            if (TryMove(currentCoordinates[0], currentCoordinates[1] - 1, mapCharArray) == true)
                 currentCoordinates[1]--;
             break;
         case ConsoleKey.DownArrow:
-            if (TryMove(currentCoordinates[0], currentCoordinates[1] + 1, mapRows) == true)
+            if (TryMove(currentCoordinates[0], currentCoordinates[1] + 1, mapCharArray) == true)
                 currentCoordinates[1]++;
             break;
         case ConsoleKey.LeftArrow:
-            if (TryMove(currentCoordinates[0] - 1, currentCoordinates[1], mapRows) == true)
+            if (TryMove(currentCoordinates[0] - 1, currentCoordinates[1], mapCharArray) == true)
                 currentCoordinates[0]--;
             break;
         case ConsoleKey.RightArrow:
-            if (TryMove(currentCoordinates[0] + 1, currentCoordinates[1], mapRows) == true)
+            if (TryMove(currentCoordinates[0] + 1, currentCoordinates[1], mapCharArray) == true)
                 currentCoordinates[0]++;
             break;
         default:
@@ -52,26 +57,30 @@ do {
     }
     Console.SetCursorPosition(currentCoordinates[0],currentCoordinates[1]);
 
-    // If the cursor is on top of the *, exit loop
-    if (mapRows[Console.CursorTop][Console.CursorLeft] == '*')
+    // If the cursor is on top of the # (exit) or % (bad guy), exit loop
+    if ("#%".Contains(mapCharArray[Console.CursorTop][Console.CursorLeft]) == true)
     {
+        if (mapCharArray[Console.CursorTop][Console.CursorLeft] == '#')
+            gameWon = true;
+        else if (mapCharArray[Console.CursorTop][Console.CursorLeft] == '%')
+            gameWon = false;
         Thread.Sleep(500);
         break;
     }
 } while (lastKey != ConsoleKey.Escape);
 
 // Method for checking if an attempted move is valid
-static bool TryMove(int targetX, int targetY, string[] mazeRows)
+static bool TryMove(int targetX, int targetY, char[][] grid)
 {
     if (targetX < 0)
         return false;
-    else if (targetX > mazeRows[0].Length - 1)
+    else if (targetX > grid[0].Length - 1)
         return false;
     else if (targetY < 0)
         return false;
-    else if (targetY > mazeRows.Length - 1)
+    else if (targetY > grid.Length - 1)
         return false;
-    else if (!" *".Contains(mazeRows[targetY][targetX]))
+    else if (!" #$%^".Contains(grid[targetY][targetX]))
         return false;
     else
         return true;
--- obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ff21e42b413bb2e205df74f352e90b99a3fd94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3609a360f1cdfb6c77eaf70d48edaa44891b58d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
