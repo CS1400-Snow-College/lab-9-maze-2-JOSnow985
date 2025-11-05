d1499c61e7e921be68b5b4f7b526f3f03a09254b
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-04 18:41:58 -0700

    Checkpoint 6: Move the Bad Guys
    Two new methods, badGuyMove and charLocator. charLocator searches a given array and finds a given character in it, makes an array with the indices. badGuyMove uses the coordinates of all '%' characters and uses an updated TryMove to pick a random viable spot for them to move to. They can choose the same spot and one of them ceases to exist, not sure how to fix that yet.

--- Program.cs
+++ Program.cs
@@ -37,24 +37,25 @@ do {
     switch (lastKey)
     {
         case ConsoleKey.UpArrow:
-            if (TryMove(currentCoordinates[0], currentCoordinates[1] - 1, mapCharArray) == true)
+            if (TryMove(EntityType.player, currentCoordinates[0], currentCoordinates[1] - 1, mapCharArray) == true)
                 currentCoordinates[1]--;
             break;
         case ConsoleKey.DownArrow:
-            if (TryMove(currentCoordinates[0], currentCoordinates[1] + 1, mapCharArray) == true)
+            if (TryMove(EntityType.player, currentCoordinates[0], currentCoordinates[1] + 1, mapCharArray) == true)
                 currentCoordinates[1]++;
             break;
         case ConsoleKey.LeftArrow:
-            if (TryMove(currentCoordinates[0] - 1, currentCoordinates[1], mapCharArray) == true)
+            if (TryMove(EntityType.player, currentCoordinates[0] - 1, currentCoordinates[1], mapCharArray) == true)
                 currentCoordinates[0]--;
             break;
         case ConsoleKey.RightArrow:
-            if (TryMove(currentCoordinates[0] + 1, currentCoordinates[1], mapCharArray) == true)
+            if (TryMove(EntityType.player, currentCoordinates[0] + 1, currentCoordinates[1], mapCharArray) == true)
                 currentCoordinates[0]++;
             break;
         default:
             break;
     }
+    badGuyMove(mapCharArray);
     Console.SetCursorPosition(currentCoordinates[0],currentCoordinates[1]);
 
     // If the cursor is on top of the # (exit) or % (bad guy), exit loop
@@ -70,7 +71,7 @@ do {
 } while (lastKey != ConsoleKey.Escape);
 
 // Method for checking if an attempted move is valid
-static bool TryMove(int targetX, int targetY, char[][] grid)
+static bool TryMove(EntityType entity, int targetX, int targetY, char[][] grid)
 {
     if (targetX < 0)
         return false;
@@ -80,8 +81,85 @@ static bool TryMove(int targetX, int targetY, char[][] grid)
         return false;
     else if (targetY > grid.Length - 1)
         return false;
-    else if (!" #$%^".Contains(grid[targetY][targetX]))
+    else if (entity == EntityType.player && !" #$%^".Contains(grid[targetY][targetX]))
+        return false;
+    else if (entity == EntityType.nonplayer && !" #$%".Contains(grid[targetY][targetX]))
         return false;
     else
         return true;
 }
+
+static void badGuyMove(char[][] array)
+{
+    // Get an array of all bad guys on the map
+    var badGuyLocations = charLocator(array, '%');
+
+    // Give every bad guy on the map a chance to stay still or move
+    foreach (var (row, column) in badGuyLocations)
+    {
+        int currentRow = row;
+        int currentColumn = column;
+
+        // Make a list of the potential destinations for the bad guy
+        var viableDestinations = new List<(int row, int column)>();
+
+        for (int rowToCheck = -1; rowToCheck <= 1; rowToCheck++)
+        {
+            for (int columnToCheck = -1; columnToCheck <= 1; columnToCheck++)
+            {
+                int targetRow = currentRow + rowToCheck;
+                int targetColumn = currentColumn + columnToCheck;
+
+                // Move method checks for 
+                if (TryMove(EntityType.nonplayer, targetColumn, targetRow, array) == false)
+                    continue;
+
+                viableDestinations.Add((targetRow, targetColumn));
+            }
+        }
+
+        // Pull a random set of coordinates out of the list of possible ones
+        var randomized = new Random();
+        var chosenDestination = viableDestinations[randomized.Next(0, viableDestinations.Count)];
+        int rowMovingTo = chosenDestination.row;
+        int columnMovingTo = chosenDestination.column;
+
+        // Erase old % and remove it from the map array
+        Console.SetCursorPosition(currentColumn, currentRow);
+        Console.Write(' ');
+        array[currentRow][currentColumn] = ' ';
+
+        // Print new % and add it to the map array
+        Console.SetCursorPosition(columnMovingTo, rowMovingTo);
+        Console.Write('%');
+        array[rowMovingTo][columnMovingTo] = '%';
+    }
+}
+
+// Find bad guys in array, 
+// check spaces around bad guys, 
+// randomly select viable spot, 
+// erase bad guy from current position in array and on board
+// write % to new position in array and on board
+// Might not need to return cursor position to player's location
+
+static (int row, int column)[] charLocator(char[][] grid, char target)
+{
+    var locations = new List<(int row, int column)>();
+
+    for (int rowIndex = 0; rowIndex < grid.Length; rowIndex++)
+    {
+        char[] row = grid[rowIndex];
+        for (int columnIndex = 0; columnIndex < row.Length; columnIndex++)
+        {
+            if (row[columnIndex] == target)
+            {
+                locations.Add((rowIndex, columnIndex));
+            }
+        }
+    }
+
+    return locations.ToArray();
+}
+
+enum EntityType {player, nonplayer}
--- obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-9-maze-2-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3609a360f1cdfb6c77eaf70d48edaa44891b58d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ecbb1a5dcf1e4294f200813a90ab987a99a533")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-9-maze-2-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
